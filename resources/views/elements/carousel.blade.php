b217a7339fbf3f95bd73f8c2ca391ffb512f2cff
Rabie <rabiakallou@gmail.com>
2020-03-01 21:00:40 +0100

    change website layaout

--- resources/views/elements/carousel.blade.php
+++ resources/views/elements/carousel.blade.php
@@ -1,28 +1,45 @@
-<section class="container">
-    <div id="slider" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#slider" data-slide-to="0" class="active"></li>
-            <li data-target="#slider" data-slide-to="1"></li>
-            <li data-target="#slider" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner mt-300">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="https://source.unsplash.com/random" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://via.placeholder.com/400" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://via.placeholder.com/400" alt="Third slide">
-            </div>
+
+  <div id="slider" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#slider" data-slide-to="0" class="active"></li>
+      <li data-target="#slider" data-slide-to="1"></li>
+      <li data-target="#slider" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img src="https://i.postimg.cc/wTBDN2JW/1.jpg" alt="...">
+        <div class="carousel-caption">
+          <h2 class="animated bounceInRight" style="animation-delay: 1s">We Are Reliable</h2>
+          <h3 class="animated bounceInLeft" style="animation-delay: 2s">Lorem ipsum dolor sit amet.</h3>
         </div>
-        <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+      </div>
+      <div class="carousel-item">
+        <img src="https://i.postimg.cc/GhHwf0Gv/2.jpg" alt="...">
+        <div class="carousel-caption">
+          <h2 class="animated slideInDown" style="animation-delay: 1s">We Deliver On Time</h2>
+          <h3 class="animated slideInRight" style="animation-delay: 2s">Lorem ipsum dolor sit amet.</h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://i.postimg.cc/ncsgk4fk/3.jpg" alt="...">
+        <div class="carousel-caption">
+          <h2 class="animated zoomIn" style="animation-delay: 1s">Best Customer Support</h2>
+          <h3 class="animated zoomIn" style="animation-delay: 2s">Lorem ipsum dolor sit amet.</h3>
+        </div>
+      </div>
+
     </div>
-</section>
+
+    <!-- Controls -->
+    <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+  </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 
-@section('content')
+@section('slider')
     @include('elements.carousel')
 @endsection
+
