e555d18907a41a197e51a59c688c71d3826a787b
Rabie <rabiakallou@gmail.com>
2020-03-12 23:51:34 +0100

    add departments section

---
+++ resources/views/departments/index.blade.php
@@ -0,0 +1,43 @@
+<section id="departments" class="mt-2 py container home__info">
+    <h3 class="text-center">Departements</h3>
+    <hr class="sep">
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="card card-block p-2">
+                <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg" alt="Photo of sunset">
+                <h5 class="card-title mt-1 mb-1">Sierra Web Development </h5>
+                <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+                <a class="btn btn-secondary stretched-link" href="#">Read more</a>
+
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="card card-block p-2">
+                <img src="https://static.pexels.com/photos/7357/startup-photos.jpg" alt="Photo of sunset">
+                <h5 class="card-title mt-1 mb-1">Sierra Web Development </h5>
+                <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+                <a class="btn btn-secondary stretched-link" href="#">Read more</a>
+
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="card card-block p-2">
+                <img src="https://static.pexels.com/photos/326424/pexels-photo-326424.jpeg" alt="Photo of sunset">
+                <h5 class="card-title mt-1 mb-1">Sierra Web Development </h5>
+                <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+                <a class="btn btn-secondary stretched-link" href="#">Read more</a>
+
+            </div>
+        </div>
+        <div  class="col-md-3 col-sm-6">
+            <div class="card card-block p-2">
+                <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg" alt="Photo of sunset">
+                <h5 class="card-title mt-1 mb-1">Sierra Web Development </h5>
+                <p class="card-text">This is a company that builds websites, web apps and e-commerce solutions.</p>
+                <a class="btn btn-secondary stretched-link" href="#">Read more</a>
+
+            </div>
+        </div>
+    </div>
+
+</section>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 
 Auth::routes(['register'=>false]);
- Route::get('/home', 'HomeController@index')->name('home');
+ Route::get('/home', 'HomeController@index');
 // Route::get('/login', 'HomeController@index')->name('home');
 Route::view('/etudiant', 'students.index')->name('students.index');
 Route::view('/enseignant', 'teachers.teacher');
