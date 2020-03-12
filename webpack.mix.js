109792ea403e5d74cb0af33efa76a1fb454d41df
Rabie <rabiakallou@gmail.com>
2020-03-12 17:08:56 +0100

    fix sidebar

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 mix.browserSync({
-    proxy: 'www.school.test',
+    proxy: 'school.test',
     port: 5973,
-    https: true
 });
