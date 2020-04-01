9756a41958f5ea6fc5e225cefcd66a78afb03e8a
Rabie <rabiakallou@gmail.com>
2020-04-01 21:05:53 +0100

    migrate & seed the database

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         @foreach ($departments as $dept)
         <div class="col-md-3 col-sm-6">
-            <div class="card card-block p-2 mh-3">
+            <div class="card card-block p-2">
                 <img src="https://static.pexels.com/photos/7096/people-woman-coffee-meeting.jpg" alt="Photo of sunset">
                 <h5 class="card-title mt-1 mb-1">{{ $dept->dept_name }} </h5>
             <p class="card-text">{{ $dept->dept_desc }}</p>
